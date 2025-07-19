243c1d0ec5484a1839b853adeb740b6b28a5623d
Naman Pandey <2492004namanpandey@gmail.com>
2025-07-19 12:10:05 +0530

    feat: react-native intiallizaton

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['module:react-native-dotenv', {
+      "env": {
+        "development": {
+          "moduleName": "@env",
+          "path": ".env",
+          "blocklist": null,
+          "allowlist": null,
+          "safe": false,
+          "allowUndefined": true,
+          "verbose": false
+        },
+        "production": {
+          "moduleName": "@env",
+          "path": ".env",
+          "blocklist": null,
+          "allowlist": null,
+          "safe": false,
+          "allowUndefined": true,
+          "verbose": false
+        }
+      }
+    }]
+  ]
 };
